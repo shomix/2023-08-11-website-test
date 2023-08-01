4d647357962319ea72f51125cf0624b6654af4ce
caleb <calebakpan7@gmail.com>
2023-08-01 15:08:48 +0100

    feat: worked on pricingPage

--- components/assets/index.js
+++ components/assets/index.js
@@ -11,8 +11,15 @@ import Linkedin from './icons/linkedin.svg';
 import Instagram from './icons/instagram.svg';
 import Discord from './icons/discord.svg';
 
+// --- Images ---
+import Frame4 from './pricing_page_assests/Frame4.png';
+import Line6 from './pricing_page_assests/Line 6.png';
+import messages2 from './pricing_page_assests/messages-2.png';
+import tickCircle from './pricing_page_assests/tick-circle.svg';
+
 // --- Exports ---
 export default Logo;
+export { Frame4, Line6, messages2, tickCircle };
 export {
   PlusCircle,
   MinusCircle,
